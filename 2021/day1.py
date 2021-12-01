f6092928fe29e332481db241aed68532896ad2a8
loboyd <logan.boyd@protonmail.com>
2021-12-01 11:05:06 -0500

    Update day1.py

--- 2021/day1.py
+++ 2021/day1.py
@@ -8,5 +8,4 @@ f.close()
 print(sum([1 if u < v else 0 for u,v in zip(data, data[1:])]))
 
 # part 2
-data = [x+y+z for x,y,z in zip(data, data[1:], data[2:])]
-print(sum([1 if u < v else 0 for u,v in zip(data, data[1:])]))
+print(sum([1 if u < v else 0 for u,v in zip(data, data[3:])]))
